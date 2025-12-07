5053d68d4b97027b732b8de13407b0d0f44a354c
saturnvi <maxwang420@gmail.com>
2025-12-06 18:35:35 -0600

    play a little golf

--- day1/main.py
+++ day1/main.py
@@ -1,37 +1,21 @@
 instructions = []
 
+p1sum = 0
+p2sum = 0
+number = 50
+
 with open("1.input") as f:
-    lines = f.readlines()
+    for instruction in [int(line.replace("R", "").replace("L", "-")) for line in f.readlines()]:
+        mirror = instruction < 0
+        if mirror and number > 0:
+            number = 100 - number
+        number += abs(instruction)
+        p2sum += number // 100
+        number = number % 100
+        if mirror and number > 0:
+            number = 100 - number
+        if number == 0:
+            p1sum += 1
 
-    for line in lines:
-        if "R" in line:
-            instructions.append(int(line.replace("R", "")))
-        else:
-            instructions.append(int(line.replace("L", "-")))
-
-zero_count = 0
-number = 50
-
-# part 1
-for instruction in instructions:
-    number += instruction
-    if number == 0:
-        zero_count += 1
-
-zero_passed_count = 0
-number = 50
-
-# part 2
-for instruction in instructions:
-    mirror = instruction < 0
-    if mirror and number > 0:
-        number = 100 - number
-    number += abs(instruction)
-    zero_passed_count += number // 100
-    number = number % 100
-    if mirror and number > 0:
-        number = 100 - number
-
-
-print(f"Part 1: {zero_count}")
-print(f"Part 2: {zero_passed_count}")
+print(f"Part 1: {p1sum}")
+print(f"Part 2: {p2sum}")
